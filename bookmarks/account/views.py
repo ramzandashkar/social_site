16654666e03b813b7dbf67f1fd0bc0dc4aedb7af
ramzandashkar <dashkarov@gmail.com>
2023-06-13 17:59:38 +0300

    Add authenticated page

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -28,3 +29,6 @@ def user_login(request):
     return render(request, 'account/login.html', {'form': form})
 
 
+@login_required
+def dashboard(request):
+    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
