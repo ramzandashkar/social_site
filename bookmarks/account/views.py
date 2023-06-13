c5c2f13aeca9c4629132aa4a6ff3f1a05e58c09c
ramzandashkar <dashkarov@gmail.com>
2023-06-13 16:53:23 +0300

    Начало

---
+++ bookmarks/account/views.py
@@ -0,0 +1,30 @@
+from django.contrib.auth import login, authenticate
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from .forms import LoginForm
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(
+                request,
+                username=cd['username'],
+                password=cd['password'],
+            )
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Аутентификация прошла успешно')
+                else:
+                    return HttpResponse('Аккаунт отключен')
+            else:
+                return HttpResponse('Invalid login')
+    else:
+        form = LoginForm()
+    return render(request, 'account/login.html', {'form': form})
+
+
