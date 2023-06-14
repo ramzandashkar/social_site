14dd83618333aa5d8ad9709aabbbae164a9106bb
ramzandashkar <dashkarov@gmail.com>
2023-06-14 10:40:06 +0300

    Add profile account

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -3,7 +3,13 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from .forms import LoginForm, UserRegistrationForm
+from .forms import (
+    LoginForm,
+    UserRegistrationForm,
+    UserEditForm,
+    ProfileEditForm,
+)
+from .models import Profile
 
 
 def user_login(request):
@@ -38,6 +44,7 @@ def register(request):
                 user_form.cleaned_data['password']
             )
             new_user.save()
+            Profile.objects.create(user=new_user)
             return render(request, 'account/template/register_done.html', {'new_user': new_user})
     else:
         user_form = UserRegistrationForm()
@@ -47,3 +54,21 @@ def register(request):
 @login_required
 def dashboard(request):
     return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(
+            instance=request.user.profile,
+            data=request.POST,
+            files=request.FILES
+        )
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+    return render(request, 'account/edit.html', {'user_form': user_form, 'profile_form': profile_form})
