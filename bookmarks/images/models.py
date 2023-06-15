b258e3cac242ce0040acd5ee74b4008678663f61
ramzandashkar <dashkarov@gmail.com>
2023-06-15 14:06:03 +0300

    Add bookmark for images and load image from other web sites

--- bookmarks/images/models.py
+++ bookmarks/images/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.conf import settings
+from django.urls import reverse
+from django.utils.text import slugify
 
 
 class Image(models.Model):
@@ -17,12 +19,26 @@ class Image(models.Model):
     image = models.ImageField(upload_to='images/%Y/%m/%d/')
     description = models.TextField(blank=True)
     created = models.DateField(auto_now_add=True)
+    users_like = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name='images_liked',
+        blank=True,
+    )
 
     class Meta:
         indexes = [
             models.Index(fields=['-created']),
         ]
         ordering = ['-created']
+
+    # переопределения метода save() для автоматичекого заполнения поля slug
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('images:detail', args=[self.id, self.slug])
 
     def __str__(self):
         return self.title
