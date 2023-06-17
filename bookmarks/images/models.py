344947fddb6907f0c5d7a6db844d5aa432857fc8
Рамзан Дашкаров <dashkarov@gmail.com>
2023-06-18 01:48:50 +0300

    Исправление кода, который удалил мальчик

--- bookmarks/images/models.py
+++ bookmarks/images/models.py
@@ -35,8 +35,8 @@ class Image(models.Model):
     # переопределения метода save() для автоматичекого заполнения поля slug
     def save(self, *args, **kwargs):
         if not self.slug:
-6
-super().save(*args, **kwargs)
+            self.slug = slugify(unidecode(self.title))
+        super().save(*args, **kwargs)
 
     def get_absolute_url(self):
         return reverse('images:detail', args=[self.id, self.slug])
