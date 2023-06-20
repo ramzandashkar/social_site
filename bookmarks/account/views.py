39a7b8e6f8d633df66506acee348a0e579c54da6
Рамзан Дашкаров <dashkarov@gmail.com>
2023-06-20 09:00:31 +0300

    Update project

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -55,14 +55,13 @@ def register(request):
 
 @login_required
 def dashboard(request):
-    # Display all actions by default
     actions = Action.objects.exclude(user=request.user)
     following_ids = request.user.following.values_list('id', flat=True)
     if following_ids:
-        # If user is following others, retrieve only their actions
         actions = actions.filter(user_id__in=following_ids)
     actions = actions.select_related('user', 'user__profile').prefetch_related('target')[:10]
     return render(request, 'account/dashboard.html', {'section': 'dashboard', 'actions': actions})
+
 
 @login_required
 def edit(request):
